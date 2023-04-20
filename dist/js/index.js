cc34cd288c5d47a446baa8b4e7cec3015cd0c0f9
Michael Loomis <85528807+labordude@users.noreply.github.com>
2023-04-19 22:15:40 -0500

    building cards

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,116 +1,175 @@
-addEventListener('DOMContentLoaded', () => {
-  /**********DELIVERABLES START*****************/
+/**********DELIVERABLES START*****************/
 
-  /**********DELIVERABLES END*******************/
+/**********DELIVERABLES END*******************/
 
-  /**********VARIABLE DECLARATION START*********/
+/**********VARIABLE DECLARATION START*********/
 
-  const endpoints = {
-    users: 'users',
-    games: 'games',
-    platforms: 'platforms',
-    genres: 'genres',
-    developers: 'developers',
-    favorites: 'favorites',
-    collections: 'collections',
-  };
-  
-  const rawgUrl = `https://api.rawg.io/api/`;
-  
-  const gamesList = document.querySelector('#games-list');
-  const gameDetails = document.querySelector('#game-details');
-  let currentGame;
-  /**********VARIABLE DECLARATION END***********/
+const endpoints = {
+  users: 'users',
+  games: 'games',
+  platforms: 'platforms',
+  genres: 'genres',
+  developers: 'developers',
+  favorites: 'favorites',
+  collections: 'collections',
+};
 
-  /**********FETCH REQUESTS START***************/
-  const rover = {
-    get: function getJSON(url) {
-      return fetch(url)
-        .then((response) => {
-          if (response.ok) {
-            // console.log(response.json());
-            return response.json();
-          } else {
-            throw response.statusText;
-          }
-        })
-        .catch((error) => console.log(error.message));
-    },
+const rawgUrl = `https://api.rawg.io/api/`;
+const gamesList = document.querySelector('#games-list');
+const gameDetails = document.querySelector('#game-details');
 
-    patch: function postJSON(url, data) {
-      return fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
+let currentGame;
+/**********VARIABLE DECLARATION END***********/
+
+/**********FETCH REQUESTS START***************/
+const rover = {
+  get: function getJSON(url) {
+    return fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          // console.log(response.json());
+          return response.json();
+        } else {
+          throw response.statusText;
+        }
       })
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw response.statusText;
-          }
-        })
-        .catch((error) => console.log(error.message));
-    },
-  };
-  /**********FETCH REQUESTS END*****************/
+      .catch((error) => console.log(error.message));
+  },
 
-  /**********EVENT LISTENERS START**************/
+  patch: function postJSON(url, data) {
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw response.statusText;
+        }
+      })
+      .catch((error) => console.log(error.message));
+  },
+};
+/**********FETCH REQUESTS END*****************/
 
-  /**********EVENT LISTENERS END****************/
+/**********EVENT LISTENERS START**************/
 
-  /**********FORM PROCESSING START**************/
-  //handle input from search form
-  function handleForm(event) {
-    event.preventDefault();
-    console.log(event);
-  }
+/**********EVENT LISTENERS END****************/
 
-  function handleClick(event) {
-    console.log(event);
-  }
-  /**********FORM PROCESSING END****************/
+/**********FORM PROCESSING START**************/
+//handle input from search form
+function handleForm(event) {
+  event.preventDefault();
+  console.log(event);
+}
 
-  /**********DOM RENDER FUNCTIONS START*********/
-  function renderAllImages(characters) {
-    characters.data.results.forEach((character) => renderImage(character));
-  }
+function handleClick(event) {
+  console.log(event);
+}
+/**********FORM PROCESSING END****************/
 
-  function renderImage(game) {
-    //create elements
-    let gameImage = document.createElement('img');
+/**********DOM RENDER FUNCTIONS START*********/
+function renderAllImages(characters) {
+  characters.data.results.forEach((character) => renderImage(character));
+}
 
-    //populate attributes, classes, and text
+function renderGameCards(game) {
+  //create elements
+  let gameCardDiv = document.createElement('div');
+  let gameCardImageAnchor = document.createElement('a');
+  let gameImage = document.createElement('img');
+  let gameCardDetailsDiv = document.createElement('div');
+  let gameCardTitleDiv = document.createElement('div');
+  let gameCardTitleAnchor = document.createElement('a');
+  let gameCardTitle = document.createElement('h5');
+  let gameCardSubDetailsDiv = document.createElement('div');
+  let gameMyRatingDiv = document.createElement('p');
+  let gameCardRatingDiv = document.createElement('div');
+  let gameCardBorderDiv = document.createElement('div');
+  let gameCardGenresDiv = document.createElement('div');
+  let gameCardGenresList = document.createElement('ul');
 
-    gameImage.src = game.background_image;
-    console.log(gameImage.src);
-    //append to DOM
-    gamesList.append(gameImage);
-  }
+  //populate attributes, classes, and text
+  gameCardDiv.classList.add('game-card-div');
+  gameImage.classList.add('game-card-image');
+  gameCardDetailsDiv.classList.add('game-card-details');
+  gameCardSubDetailsDiv.classList.add('game-card-sub-details');
+  gameMyRatingDiv.classList.add('game-card-rating');
+  gameCardGenresDiv.classList.add('game-card-genres');
+  gameImage.classList.add('game-card-image');
+  gameImage.src = game.background_image;
+  gameCardTitle.textContent = game.name;
+  gameCardRatingDiv.textContent = `Rating: ${game.rating} / ${game.rating_top} | My rating:${game.user_rating}`;
+  gameCardGenresDiv.textContent = 'Genres:';
+  game.genres.forEach((genre) => {
+    createLinkList(genre, gameCardGenresList);
+  });
 
-  /**********DOM RENDER FUNCTIONS END***********/
+  currentGame = game;
 
-  /***********GENERAL FUNCTIONS START***********/
-  function getAll() {
-    rover
-      .get(
-        `${rawgUrl}${endpoints.users}/${config.me}/${endpoints.games}?statuses=playing&key=${config.apikey}`
-      )
-      .then((games) => {
-        console.log(games);
-        games.results.map((game) => {
-          renderImage(game);
-        });
+  //append to DOM
+  gameCardGenresDiv.append(gameCardGenresList);
+  gameCardRatingDiv.append(gameMyRatingDiv);
+  gameCardSubDetailsDiv.append(gameCardRatingDiv, gameCardGenresDiv);
+  gameCardTitleAnchor.append(gameCardTitle);
+  gameCardDetailsDiv.append(gameCardTitleAnchor, gameCardSubDetailsDiv);
+  gameCardImageAnchor.append(gameImage);
+  gameCardDiv.append(gameCardImageAnchor, gameCardDetailsDiv);
+  gamesList.appendChild(gameCardDiv);
+  gameImage.addEventListener('click', () => showGameDetails(game));
+}
+
+function showGameDetails(game) {
+  // identify
+  let gameImage = document.querySelector('#game-details #selected-game-image');
+  let gameTitle = document.querySelector('#game-details #selected-game-title');
+  let gameReleaseDate = document.querySelector(
+    '#game-details #selected-game-release-date'
+  );
+  //create elements
+
+  //populate data, text, classes, attributes
+  gameImage.src = game.background_image;
+  gameTitle.textContent = game.name;
+  gameReleaseDate.textContent = game.released;
+
+  //append elements to the DOM
+}
+
+/**********DOM RENDER FUNCTIONS END***********/
+
+/***********GENERAL FUNCTIONS START***********/
+function getMyCurrentGames() {
+  rover
+    .get(
+      `${rawgUrl}${endpoints.users}/${config.me}/${endpoints.games}?statuses=playing&key=${config.apikey}`
+    )
+    .then((games) => {
+      games.results.map((game) => {
+        currentGame = game;
+        renderGameCards(game);
       });
-  }
+    });
+}
 
-  function getOne() {
-    getJSON;
-  }
-  getAll();
+function getOne() {
+  getJSON;
+}
+getMyCurrentGames();
 
-  function my(section = '') {}
-  /***********GENERAL FUNCTIONS END*************/
-});
+function my(section = '') {}
+
+function createLinkList(input, appendTo) {
+  let li = document.createElement('li');
+  let link = document.createElement('a');
+  link.href = '#genres';
+  link.textContent = input.name;
+  li.appendChild(link);
+  li.addEventListener('click', () => console.log(input));
+  appendTo.appendChild(li);
+}
+/***********GENERAL FUNCTIONS END*************/
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}', '*.{html,js}', './public/**/*.{html,js}'],
+  content: ['./src/**/*.{html,js}', '*.{html,js}', './dist/**/*.{html,js}',"./node_modules/tw-elements/dist/js/**/*.js"],
   darkMode: 'class',
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
 };
