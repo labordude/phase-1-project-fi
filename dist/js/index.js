61141016ae17133dad00e0c59b84e611b6448168
Michael Loomis <85528807+labordude@users.noreply.github.com>
2023-04-20 22:28:26 -0500

    images showing, tailwind card built

--- dist/js/index.js
+++ dist/js/index.js
@@ -79,47 +79,98 @@ function renderAllImages(characters) {
 
 function renderGameCards(game) {
   //create elements
-  let gameCardDiv = document.createElement('div');
-  let gameCardImageAnchor = document.createElement('a');
-  let gameImage = document.createElement('img');
-  let gameCardDetailsDiv = document.createElement('div');
-  let gameCardTitleDiv = document.createElement('div');
-  let gameCardTitleAnchor = document.createElement('a');
-  let gameCardTitle = document.createElement('h5');
-  let gameCardSubDetailsDiv = document.createElement('div');
-  let gameMyRatingDiv = document.createElement('p');
-  let gameCardRatingDiv = document.createElement('div');
-  let gameCardBorderDiv = document.createElement('div');
-  let gameCardGenresDiv = document.createElement('div');
-  let gameCardGenresList = document.createElement('ul');
 
-  //populate attributes, classes, and text
-  gameCardDiv.classList.add('game-card-div');
-  gameImage.classList.add('game-card-image');
-  gameCardDetailsDiv.classList.add('game-card-details');
-  gameCardSubDetailsDiv.classList.add('game-card-sub-details');
-  gameMyRatingDiv.classList.add('game-card-rating');
-  gameCardGenresDiv.classList.add('game-card-genres');
+  //   <div className="game-card">
+  let gameCard = document.createElement('div');
+  gameCard.classList.add('game-card');
+  //   <img src="https://media.rawg.io/media/games/bf7/bf73b105ccbba42107986bbcd96fcada.jpg" />
+  let gameImage = document.createElement('img');
   gameImage.classList.add('game-card-image');
   gameImage.src = game.background_image;
+  //   <h3 className="game-card-title">Horizon Forbidden West</h3>
+  let gameCardTitle = document.createElement('h3');
   gameCardTitle.textContent = game.name;
-  gameCardRatingDiv.textContent = `Rating: ${game.rating} / ${game.rating_top} | My rating:${game.user_rating}`;
-  gameCardGenresDiv.textContent = 'Genres:';
+  //   <div className="card-bottom">
+  let gameCardBottom = document.createElement('div');
+  gameCardBottom.classList.add('card-bottom');
+  //     <div className="card-bottom-left">
+  let gameCardBottomLeft = document.createElement('div');
+  gameCardBottomLeft.classList.add('card-bottom-left');
+  //       <div className="info-group">
+  let gameCardBottomLeftRating = document.createElement('div');
+  gameCardBottomLeftRating.classList.add('info-group');
+  //         <FontAwesomeIcon icon={faStar} className="icon" />
+  let gameCardRatingIcon = document.createElement('i');
+  gameCardRatingIcon.classList.add('fa-solid', 'fa-star');
+  //         <p>4.75 / 5</p>
+  let gameCardRating = document.createElement('p');
+  gameCardRating.textContent = `Rating: ${game.rating} / ${game.rating_top}`;
+  //       </div>
+  //       <div className="info-group">
+  let gameCardBottomLeftGenre = document.createElement('div');
+  gameCardBottomLeftGenre.classList.add('info-group');
+  //         <FontAwesomeIcon icon={faGamepad} className="icon" />
+  let gameCardGenreIcon = document.createElement('i');
+  gameCardGenreIcon.classList.add('fa-solid', 'fa-gamepad');
+  //         <p>Action Action RPG</p>
+  let gameCardGenres = document.createElement('p');
+  gameCardGenres.classList.add('game-card-genres');
+  let gameCardGenresList = document.createElement('ul');
   game.genres.forEach((genre) => {
     createLinkList(genre, gameCardGenresList);
   });
+  //       </div>
+  //     </div>
+  //     <div className="card-bottom-right">
+  let gameCardBottomRight = document.createElement('div');
+  gameCardBottomRight.classList.add('card-bottom-right');
+  //       <div className="info-group">
+  let gameCardBottomRightMyRating = document.createElement('div');
+  gameCardBottomRightMyRating.classList.add('info-group');
+  gameCardBottomRightMyRating.textContent = ``;
+  //         <FontAwesomeIcon icon={faStar} className="icon" />
+  // let gameCardRatingIcon = document.createElement('i');
+  // gameCardRatingIcon.classList.add('fa-solid', 'fa-star');
+  //         <p>4.75 / 5</p>
+  // let gameCardRating = document.createElement('p');
+  // gameCardRating.textContent = `Rating: ${game.rating} / ${game.rating_top} | My rating:${game.user_rating}`;
+  //       </div>
+  //       <div className="info-group">
+  let gameCardBottomRightGenre = document.createElement('div');
+  gameCardBottomRightGenre.classList.add('info-group');
+  //         <FontAwesomeIcon icon={faGamepad} className="icon" />
+  // let gameCardGenreIcon = document.createElement('i');
+  // gameCardGenreIcon.classList.add('fa-solid', 'fa-gamepad');
+  //         <p>Action Action RPG</p>
+  // let gameCardGenres = document.createElement('p');
+  // let gameCardGenresList = document.createElement('ul');
+  // game.genres.forEach((genre) => {
+  //   createLinkList(genre, gameCardGenresList);
+  // });
+  // </div>
+  //populate attributes, classes, and text
 
   currentGame = game;
 
+  gameCardBottomRight.append(
+    gameCardBottomRightMyRating,
+    gameCardBottomRightGenre
+  );
+  gameCardGenres.append(gameCardGenresList);
+  gameCardBottomLeftGenre.append(gameCardGenreIcon, gameCardGenres);
+  gameCardBottomLeftRating.append(gameCardRatingIcon, gameCardRating);
+  gameCardBottomLeft.append(gameCardBottomLeftRating, gameCardBottomLeftGenre);
+  gameCardBottom.append(gameCardBottomLeft, gameCardBottomRight);
+  gameCard.append(gameImage, gameCardTitle, gameCardBottom);
   //append to DOM
-  gameCardGenresDiv.append(gameCardGenresList);
-  gameCardRatingDiv.append(gameMyRatingDiv);
-  gameCardSubDetailsDiv.append(gameCardRatingDiv, gameCardGenresDiv);
-  gameCardTitleAnchor.append(gameCardTitle);
-  gameCardDetailsDiv.append(gameCardTitleAnchor, gameCardSubDetailsDiv);
-  gameCardImageAnchor.append(gameImage);
-  gameCardDiv.append(gameCardImageAnchor, gameCardDetailsDiv);
-  gamesList.appendChild(gameCardDiv);
+  // gameCardGenresDiv.append(gameCardGenresList);
+  // gameCardRatingDiv.append(gameMyRatingDiv);
+  // gameCardSubDetailsDiv.append(gameCardRatingDiv, gameCardGenresDiv);
+  // gameCardTitleAnchor.append(gameCardTitle);
+  // gameCardDetailsDiv.append(gameCardTitleAnchor, gameCardSubDetailsDiv);
+  // gameCardImageAnchor.append(gameImage);
+  // gameCardDiv.append(gameCardImageAnchor, gameCardDetailsDiv);
+  gamesList.appendChild(gameCard);
   gameImage.addEventListener('click', () => showGameDetails(game));
 }
 
@@ -146,10 +197,11 @@ function showGameDetails(game) {
 function getMyCurrentGames() {
   rover
     .get(
-      `${rawgUrl}${endpoints.users}/${config.me}/${endpoints.games}?statuses=playing&key=${config.apikey}`
+      `${rawgUrl}${endpoints.users}/${config.me}/${endpoints.games}?page_size=20&key=${config.apikey}`
     )
     .then((games) => {
-      games.results.map((game) => {
+      let gamesArray = games.results.sort((a, b) => a.playtime - b.playtime);
+      gamesArray.map((game) => {
         currentGame = game;
         renderGameCards(game);
       });
